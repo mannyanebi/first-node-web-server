d005ca33a63239546e41b7baa787e20001f4956e
mannyanebi <anebiemmanuel@gmail.com>
2018-10-17 10:55:15 +0100

    First Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+console.log('Starting Server.js');
+
+//requiring the express module (3rd Party)
+const express = require('express');
+
+let app = express();
+
+//requiring the hbs express hbs plugin
+const hbs = require('hbs')
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function (req, res, next) {
+    let now = new Date().toString();
+    console.log(`Time ${now} ${req.method} ${req.url}`);
+    next();
+});
+
+
+hbs.registerHelper('getCurrentYear', function () {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', function (text) {
+    return text.toUpperCase();
+});
+
+app.get('/', function (req, res) {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page'
+    })
+});
+
+app.get('/about', function (req, res) {
+    res.render('about.hbs', {
+        pageTitle: 'About Mustache Page'
+    })
+});
+
+app.get('/projects', function (req, res) {
+    res.render('projects.hbs', {
+        pageTitle: 'Our Portfolio'
+    })
+});
+
+app.listen(3000, function () {
+    console.log('Server has started and listening at port 3000')
+})
